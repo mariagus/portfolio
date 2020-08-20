37fb0bae48ef71eec56436a3701bcdcbe649ede1
Maria Gusova <masha.gu@gmail.com>
2020-08-21 00:23:02 +0200

    Simplify navbar scroll

--- script.js
+++ script.js
@@ -1,50 +1,23 @@
+// projects button scroll function:
+
 function scrollWhenClicked() {
-  document.querySelector(".projects").scrollIntoView();
-}
-
-function scrollAbout() {
-  document.querySelector(".about").scrollIntoView();
-}
-
-function scrollContact() {
-  document.querySelector(".contact").scrollIntoView();
-}
-
-function scrollHome() {
-  document.querySelector(".home").scrollIntoView();
-}
-
-window.onscroll = function () {
-  makeSticky();
-};
-
-var navbar = document.getElementById("myTopnav");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function makeSticky() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
+  window.location.href = "#projects";
+  document.querySelector("#projects").scrollIntoView();
 }
 
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  var x = document.getElementById("navbar");
+  if (x.className === "nav") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "nav";
   }
 }
 // Get the container element
-const topNav = document.getElementById("myTopnav");
+const navbar = document.getElementById("navbar");
 
 // Get all buttons with class="btn" inside the container
-const section = topNav.getElementsByClassName("section");
+const section = document.getElementsByClassName("section");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < section.length; i++) {
