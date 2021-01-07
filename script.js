e62da4000aee188b8d2d9f5f50b6d1ceef9e5529
Maria Gusova <masha.gu@gmail.com>
2021-01-07 21:15:36 +0000

    fix navbar

--- script.js
+++ script.js
@@ -3,12 +3,9 @@ function scrollWhenClicked() {
 }
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-  makeSticky();
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector(".nav").style.top = "0";
-  } else {
-    document.querySelector(".nav").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
 };
@@ -16,16 +13,6 @@ window.onscroll = function () {
 var navBar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
-var sticky = navBar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function makeSticky() {
-  if (window.pageYOffset >= sticky) {
-    navBar.classList.add("sticky");
-  } else {
-    navBar.classList.remove("sticky");
-  }
-}
 
 function responsiveNav() {
   var x = document.getElementById("navbar");
