d2c1af4355cd863dcbe58b1283f1ca734d3e2b23
Maria Gusova <masha.gu@gmail.com>
2021-01-07 21:22:40 +0000

    clean up

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+const section = document.getElementsByClassName("section");
+const navBar = document.getElementById("navbar");
+
 function scrollWhenClicked() {
   document.querySelector("#projects").scrollIntoView();
 }
-var prevScrollpos = window.pageYOffset;
+let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
+  let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector(".nav").style.top = "0";
   }
   prevScrollpos = currentScrollPos;
 };
-
-var navBar = document.getElementById("navbar");
 
 function responsiveNav() {
   if (navBar.className === "nav") {
@@ -20,12 +21,9 @@ function responsiveNav() {
   }
 }
 
-// Get all buttons with class="btn" inside the container
-const section = document.getElementsByClassName("section");
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < section.length; i++) {
+for (let i = 0; i < section.length; i++) {
   section[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
+    const current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
     navBar.className = "nav";
